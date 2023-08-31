6e42dce426f127e2a498ab26ea20bfe4c54816cd
Jacob Schmidt <innovativestudios@outlook.com>
2023-08-31 17:49:37 -0500

    Refactor 08312023@1750

---
+++ auth/database/MongoDB.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_URL } from "../config";
+
+const connectToMongoDB = async (): Promise<void> => {
+  try {
+    await mongoose.connect("mongodb://root:password123@mongodb:27017/sog_ms_users?authSource=admin");
+    console.log("MongoDB Connected!");
+  } catch (error) {
+    console.log(`An error occurred: ${error}`);
+    process.exit(1);
+  }
+};
+
+export default connectToMongoDB;
---
+++ auth/routes/user.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getCharacterByUserId,
+  getUserById,
+  updateUser,
+} from "../controllers/userController";
+
+const router = express.Router();
+
+router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id/character", getCharacterByUserId);
+router.get("/:id", getUserById);
+router.patch("/:id", updateUser);
+router.delete("/:id", deleteUser);
+
+export default router;
